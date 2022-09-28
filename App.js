aa17a1a1d7842848a59f821b708af558c68c0441
Justin Michele <jdm55@calvin.edu>
2022-09-28 14:43:01 -0400

    Added screens and navigation between them

--- App.js
+++ App.js
@@ -1,14 +1,64 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>Welcome to Calvin Location Guesser!!!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Start Quiz"
+        onPress={() => navigation.navigate('Q1')}
+      />
     </View>
   );
 }
+
+function QuestionsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Question:</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="See Results"
+        onPress={() => navigation.navigate('R1')}
+      />
+    </View>
+  );
+}
+
+function ResultsScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Result:</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="Go Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Q1" component={QuestionsScreen} />
+        <Stack.Screen name="R1" component={ResultsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
