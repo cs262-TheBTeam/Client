53f53ab717c15af342d34c6226975a5a2cf1a04c
Dani <danielacoguz@gmail.com>
2022-11-17 16:47:47 -0500

    leaderboard updates

--- screens/leaderboard.jsx
+++ screens/leaderboard.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Button, Text, TouchableHighlight, View, FlatList } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-const data = [
-    {
-        name: 'Maxwell Traincar',
-        score: 30,
-    },
-    {
-        name: 'Literally Dracula',
-        score: 100,
-    },
-    {
-        name: 'Amelia Earhart',
-        score: 70,
-    },
-    {
-        name: 'Steve From Minecraft',
-        score: 88,
-    },
-    {
-        name: 'Jefferson Steelflex',
-        score: 43,
-    },
-    {
-        name: 'That one guy in the mejier parking lot',
-        score: 0,
-    },
-];
+// const data = [
+//     {
+//         name: 'Maxwell Traincar',
+//         score: 30,
+//     },
+//     {
+//         name: 'Literally Dracula',
+//         score: 100,
+//     },
+//     {
+//         name: 'Amelia Earhart',
+//         score: 70,
+//     },
+//     {
+//         name: 'Steve From Minecraft',
+//         score: 88,
+//     },
+//     {
+//         name: 'Jefferson Steelflex',
+//         score: 43,
+//     },
+//     {
+//         name: 'That one guy in the mejier parking lot',
+//         score: 0,
+//     },
+// ];
 
 const Item = ({ name, score }) => (
     <View style={globalStyles.leaderboardCell} >
@@ -36,12 +37,29 @@ const Item = ({ name, score }) => (
 );
 
 export default function LeaderboardScreen({ route, navigation }) {
+    const [data, setData] = useState([]);
+    const getPlayers = async () => {
+        try {
+            const response = await fetch ('https://calvin-location-guesser.herokuapp.com/players');
+            const json = await response.json();
+            setData(json);
+        } catch (error) {
+            console.error(error);
 
+        }
+    }
     const { playerScore } = route.params
 
     const renderItem = ({ item }) => (
-        <Item name={item.name} score={item.score} />
+        <Item name={item.name} score={item.score} 
+        
+        />
+
     );
+    useEffect(() => {
+        getPlayers();
+    }, []);
+
 
     return (
         <View style={globalStyles.leaderboard}>
